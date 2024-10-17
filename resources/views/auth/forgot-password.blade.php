bca8a85204361531af5581977d925ef29b3d2ccc
Angel Leonardo Bianco <angel.leonardo.bianco@gmail.com>
2024-10-17 12:59:44 -0300

    "Welcome Page v 0.6"

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -34,8 +34,8 @@
 
             </div>
         </form>
-        <div class="flex items-center justify-end mt-4">
-            <a href="javascript:window.history.back();" class="ms-4 text-white underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800 w-full text-center">
+        <div class="flex items-center justify-center mt-4 w-full">
+            <a href="javascript:window.history.back();" class="ms-4 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800 w-full text-center">
                 {{ __('Volver') }}
             </a>
         </div>
