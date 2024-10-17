86057e1c6cf47584277e0cddab59edbf966baca0
Angel Leonardo Bianco <angel.leonardo.bianco@gmail.com>
2024-10-17 12:46:30 -0300

    "Welcome Page v 0.6"

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -60,7 +60,7 @@
 
         <div class="flex items-center justify-end mt-4">
             <div class="flex items-center justify-end mt-4">
-                aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
                 <a href="javascript:window.history.back();" class="ms-4  hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800 w-full text-center">
                     {{ __('Volver') }}
                 </a>
