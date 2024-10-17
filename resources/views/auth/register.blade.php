f77714ebf8e1e3d6c87410bb6e4654cb081ec83d
Angel Leonardo Bianco <angel.leonardo.bianco@gmail.com>
2024-10-17 12:48:30 -0300

    "Welcome Page v 0.6"

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -58,13 +58,12 @@
             </div>
         </form>
 
-        <div class="flex items-center justify-end mt-4">
-            <div class="flex items-center justify-end mt-4">
 
-                <a href="javascript:window.history.back();" class="ms-4  hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800 w-full text-center">
-                    {{ __('Volver') }}
-                </a>
-            </div>
+        <div class="flex items-center justify-center mt-4 w-full">
+            <a href="javascript:window.history.back();" class="ms-4 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800 w-full text-center">
+                {{ __('Volver') }}
+            </a>
         </div>
+
     </x-authentication-card>
 </x-guest-layout>
