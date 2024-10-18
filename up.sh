772a30aeaa556fe841ab812d19fc257ce761b359
Angel Leonardo Bianco <angel.leonardo.bianco@gmail.com>
2024-10-18 13:10:01 +0100

    roles & permisions v 0.1

---
+++ up.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Check if the user has root privileges
+if [[ ! $(id -u) -eq 0 ]]; then
+    echo "This script requires root privileges. Please run it with sudo."
+    exit 1
+fi
+
+# Execute the commands
+sudo git pull
+sudo php artisan route:cache
+sudo php artisan config:cache
+sudo php artisan view:cache
