38e811867af8aafbaf86ebac960702a84fb734e2
Angel Leonardo Bianco <angel.leonardo.bianco@gmail.com>
2024-10-18 13:42:06 -0300

    "Fixing Nav Bar v 0.1.1"

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link href="{{ route('roles.index') }}" :active="request()->routeIs('roles.index')">
+                        {{ __('Roles y Permisos') }}
+                    </x-nav-link>
                 </div>
             </div>
 
