e5731056e58a96e61b9acc0d7e9ab071cfc73e1d
Angel Leonardo Bianco <angel.leonardo.bianco@gmail.com>
2024-10-17 22:56:19 -0300

    "Profile Page v 0.6"

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,7 +20,7 @@ class User extends Authenticatable
     use Notifiable;
     use TwoFactorAuthenticatable;
 
-    use HasRoles;
+    //use HasRoles;
 
     /**
      * The attributes that are mass assignable.
