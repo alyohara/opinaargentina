401cae5d5a80c1c9fd06cd5bc316762b905b94b2
Angel Leonardo Bianco <angel.leonardo.bianco@gmail.com>
2024-10-21 10:44:49 -0300

    roles v 0.1

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,8 @@ use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Jetstream\HasTeams;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+
 
 class User extends Authenticatable
 {
@@ -21,6 +23,8 @@ class User extends Authenticatable
     use HasTeams;
     use Notifiable;
     use TwoFactorAuthenticatable;
+    use HasRoles;
+
 
     /**
      * The attributes that are mass assignable.
