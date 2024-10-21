ff18f58d8af00d4a1f2b16995c1a21a8fd0372a6
Angel Leonardo Bianco <angel.leonardo.bianco@gmail.com>
2024-10-21 08:53:02 -0300

    new logo fix height

--- resources/views/components/application-mark.blade.php
+++ resources/views/components/application-mark.blade.php
@@ -1,1 +1,13 @@
-<svg height="200px" width="200px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path style="fill:#25B6D2;" d="M218.944,400.496h265.52c15.2,0,27.536-12.64,27.536-28.224V28.224C512,12.64,499.664,0,484.464,0 H27.536C12.336,0,0,12.64,0,28.224v344.24c0,15.584,12.336,28.224,27.536,28.224H80.64L47.248,512l156.608-106.88 C208.336,402.112,213.584,400.496,218.944,400.496z"></path> <g> <circle style="fill:#FFFFFF;" cx="256" cy="137.984" r="57.136"></circle> <path style="fill:#FFFFFF;" d="M346.464,312.96c-0.992-54.576-41.072-98.496-90.464-98.496s-89.472,43.92-90.464,98.496H346.464z"></path> </g> </g></svg>
+<svg height="75px" width="75px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+     xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve" fill="#000000"><g
+        id="SVGRepo_bgCarrier" stroke-width="0"></g>
+    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+    <g id="SVGRepo_iconCarrier">
+        <path style="fill:#25B6D2;"
+              d="M218.944,400.496h265.52c15.2,0,27.536-12.64,27.536-28.224V28.224C512,12.64,499.664,0,484.464,0 H27.536C12.336,0,0,12.64,0,28.224v344.24c0,15.584,12.336,28.224,27.536,28.224H80.64L47.248,512l156.608-106.88 C208.336,402.112,213.584,400.496,218.944,400.496z"></path>
+        <g>
+            <circle style="fill:#FFFFFF;" cx="256" cy="137.984" r="57.136"></circle>
+            <path style="fill:#FFFFFF;"
+                  d="M346.464,312.96c-0.992-54.576-41.072-98.496-90.464-98.496s-89.472,43.92-90.464,98.496H346.464z"></path>
+        </g>
+    </g></svg>
