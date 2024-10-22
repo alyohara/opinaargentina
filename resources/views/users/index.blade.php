8c39587b86a70941c073427550fe4e9d1e5582c6
Angel Leonardo Bianco <angel.leonardo.bianco@gmail.com>
2024-10-22 14:20:16 -0300

    nav

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -8,8 +8,8 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
-            <div style="box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;">
-                <a href="{{ route('users.create') }}" class="inline-flex items-center px-4 py-2 bg-blue-500 border border-transparent rounded-md font-semibold text-white uppercase tracking-widest hover:bg-blue-700 active:bg-blue-900 focus:outline-none focus:border-blue-900 focus:ring focus:ring-blue-300 disabled:opacity-25 transition">Add User</a>
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-6">
+                <a href="{{ route('users.create') }}" box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;>Add User</a>
                 <table class="min-w-full mt-4">
                     <thead>
                     <tr>
