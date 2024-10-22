92a2554f84b1a7ca6f1aa7c1d034c2a5e5d0a52d
Angel Leonardo Bianco <angel.leonardo.bianco@gmail.com>
2024-10-21 23:19:25 -0300

    users index

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -10,23 +10,34 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-6">
                 <h1 class="text-2xl font-semibold mb-4">Users</h1>
                 <a href="{{ route('users.create') }}" class="text-blue-500 hover:underline">Add User</a>
-                <ul class="mt-4">
+                <table class="min-w-full mt-4">
+                    <thead>
+                    <tr>
+                        <th class="px-6 py-3 border-b-2 border-gray-300 dark:border-gray-700">Name</th>
+                        <th class="px-6 py-3 border-b-2 border-gray-300 dark:border-gray-700">Email</th>
+                        <th class="px-6 py-3 border-b-2 border-gray-300 dark:border-gray-700">Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
                     @foreach($users as $user)
-                        <li class="mb-2">
-                            <div class="flex justify-between items-center">
-                                <span>{{ $user->name }} ({{ $user->email }})</span>
-                                <div>
-                                    <a href="{{ route('users.edit', $user) }}" class="text-blue-500 hover:underline mr-2">Edit</a>
-                                    <form action="{{ route('users.destroy', $user) }}" method="POST" style="display:inline;">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="text-red-500 hover:underline">Delete</button>
-                                    </form>
-                                </div>
-                            </div>
-                        </li>
+                        <tr>
+                            <td class="px-6 py-4 border-b border-gray-300 dark:border-gray-700">{{ $user->name }}</td>
+                            <td class="px-6 py-4 border-b border-gray-300 dark:border-gray-700">{{ $user->email }}</td>
+                            <td class="px-6 py-4 border-b border-gray-300 dark:border-gray-700">
+                                <a href="{{ route('users.edit', $user) }}" class="text-blue-500 hover:underline mr-2">Edit</a>
+                                <form action="{{ route('users.destroy', $user) }}" method="POST" style="display:inline;">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="text-red-500 hover:underline">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
                     @endforeach
-                </ul>
+                    </tbody>
+                </table>
+                <div class="mt-4">
+                    {{ $users->links() }}
+                </div>
             </div>
         </div>
     </div>
