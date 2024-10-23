95b34f91ee568968b5b5f7fb6172eba46dbbde05
Angel Leonardo Bianco <angel.leonardo.bianco@gmail.com>
2024-10-23 11:17:56 -0300

    user managment v0.5

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -9,7 +9,7 @@
         <div class="container">
             <div class="card shadow-sm">
                 <div class="card-body">
-                    <button onclick="window.location.href='{{ route('users.create') }}'" class="btn btn-success mb-3">Add User</button>
+                    <button onclick="window.location.href='{{ route('users.create') }}'" class="btn btn-success mb-3">Agregar Usuario</button>
                     <table class="table table-striped">
                         <thead>
                         <tr>
@@ -24,11 +24,11 @@
                                 <td>{{ $user->name }}</td>
                                 <td>{{ $user->email }}</td>
                                 <td>
-                                    <a href="{{ route('users.edit', $user) }}" class="btn btn-warning btn-sm">Edit</a>
-                                    <form action="{{ route('users.destroy', $user) }}" method="POST" style="display:inline;">
+                                    <a href="{{ route('users.edit', $user) }}" class="btn btn-warning btn-sm">Editar</a>
+                                    <form action="{{ route('users.destroy', $user) }}" method="POST" class="d-inline delete-form">
                                         @csrf
                                         @method('DELETE')
-                                        <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                                        <button type="button" class="btn btn-danger btn-sm delete-button">Eliminar</button>
                                     </form>
                                 </td>
                             </tr>
@@ -42,4 +42,27 @@
             </div>
         </div>
     </div>
+
+    <script>
+        document.querySelectorAll('.delete-button').forEach(button => {
+            button.addEventListener('click', function (event) {
+                event.preventDefault();
+                const form = this.closest('form');
+
+                Swal.fire({
+                    title: 'Are you sure?',
+                    text: "You won't be able to revert this!",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes, delete it!'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        form.submit();
+                    }
+                });
+            });
+        });
+    </script>
 </x-app-layout>
