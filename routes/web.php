52687027e91d4a25885feb86d74137c466ae9acf
Angel Leonardo Bianco <angel.leonardo.bianco@gmail.com>
2024-10-24 12:43:25 -0300

    permissions manage v0.2

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RolePermissionController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\PermissionController;
 
 
 Route::get('/', function () {
