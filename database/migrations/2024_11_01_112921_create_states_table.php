7fe0e4743d40f3c7d108c2403284fd346d27f3de
Angel Leonardo Bianco <angel.leonardo.bianco@gmail.com>
2024-11-01 08:37:23 -0300

    cities and states

--- database/migrations/2024_11_01_112921_create_states_table.php
+++ database/migrations/2024_11_01_112921_create_states_table.php
@@ -1,27 +1,22 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateStatesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('states', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->timestamps();
+            // Add other columns as needed
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('states');
     }
-};
+}
