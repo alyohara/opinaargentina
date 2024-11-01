5baf860b80eabed0bbc5220270b5e2432ac3ee2c
Angel Leonardo Bianco <angel.leonardo.bianco@gmail.com>
2024-11-01 11:21:15 -0300

    cities & states models

--- app/Models/State.php
+++ app/Models/State.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class State extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name'];
+
+    public function cities()
+    {
+        return $this->hasMany(City::class);
+    }
 }
