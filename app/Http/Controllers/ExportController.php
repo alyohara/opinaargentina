452d3de115c3415d118a77df56a29f44944f2ec9
Angel Leonardo Bianco <angel.leonardo.bianco@gmail.com>
2024-11-20 11:20:04 -0300

    exports list

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Export;
 use Illuminate\Http\Request;
 
 class ExportController extends Controller
 {
-    //
+    public function index()
+    {
+        $exports = Export::with('user')->orderBy('created_at', 'desc')->paginate(10);
+        return view('exports.index', compact('exports'));
+    }
 }
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                       <x-application-mark class="block h-9 w-auto" />
+                        <x-application-mark class="block h-9 w-auto" />
                     </a>
                 </div>
 
@@ -15,28 +15,13 @@
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-{{--                    <x-nav-link href="{{ route('roles.index') }}" :active="request()->routeIs('roles.index')">--}}
-{{--                        {{ __('Roles y Permisos') }}--}}
-{{--                    </x-nav-link>--}}
-{{--                    <x-nav-link href="{{ route('users.index') }}" :active="request()->routeIs('users.index')">--}}
-{{--                        {{ __('Administrar Usuarios') }}--}}
-{{--                    </x-nav-link>--}}
-{{--                    <x-nav-link href="{{ route('roles.index') }}" :active="request()->routeIs('roles.index')">--}}
-{{--                        {{ __('Administrar Roles') }}--}}
-{{--                    </x-nav-link>--}}
-{{--                    <x-nav-link href="{{ route('permissions.index') }}" :active="request()->routeIs('permissions.index')">--}}
-{{--                        {{ __('Administrar Permisos') }}--}}
-{{--                    </x-nav-link>--}}
-{{--                    <x-nav-link href="{{ route('equipos.index') }}" :active="request()->routeIs('equipos.index')">--}}
-{{--                        {{ __('Administrar Equipos') }}--}}
-{{--                    </x-nav-link>--}}
                     <x-nav-link href="{{ route('telefonos.index') }}" :active="request()->routeIs('telefonos.index')">
                         {{ __('Administrar Teléfonos') }}
                     </x-nav-link>
-
-
+                    <x-nav-link href="{{ route('exports.index') }}" :active="request()->routeIs('exports.index')">
+                        {{ __('Exported Files') }}
+                    </x-nav-link>
                 </div>
-
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ms-6">
@@ -74,58 +59,6 @@
                         </x-dropdown-link>
                     </x-slot>
                 </x-dropdown>
-                <!-- Teams Dropdown -->
-{{--                @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())--}}
-{{--                    <div class="ms-3 relative">--}}
-{{--                        <x-dropdown align="right" width="60">--}}
-{{--                            <x-slot name="trigger">--}}
-{{--                                <span class="inline-flex rounded-md">--}}
-{{--                                    <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">--}}
-{{--                                        {{ Auth::user()->currentTeam->name }}--}}
-
-{{--                                        <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">--}}
-{{--                                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />--}}
-{{--                                        </svg>--}}
-{{--                                    </button>--}}
-{{--                                </span>--}}
-{{--                            </x-slot>--}}
-
-{{--                            <x-slot name="content">--}}
-{{--                                <div class="w-60">--}}
-{{--                                    <!-- Team Management -->--}}
-{{--                                    <div class="block px-4 py-2 text-xs text-gray-400">--}}
-{{--                                        {{ __('Manejar Equipo') }}--}}
-{{--                                    </div>--}}
-
-{{--                                    <!-- Team Settings -->--}}
-{{--                                    <x-dropdown-link href="{{ route('teams.show', Auth::user()->currentTeam->id) }}">--}}
-{{--                                        {{ __('Configuración') }}--}}
-{{--                                    </x-dropdown-link>--}}
-
-{{--                                    @can('create', Laravel\Jetstream\Jetstream::newTeamModel())--}}
-{{--                                        <x-dropdown-link href="{{ route('teams.create') }}">--}}
-{{--                                            {{ __('Crear Nuevo Equipo') }}--}}
-{{--                                        </x-dropdown-link>--}}
-{{--                                    @endcan--}}
-
-{{--                                    <!-- Team Switcher -->--}}
-{{--                                    @if (Auth::user()->allTeams()->count() > 1)--}}
-{{--                                        <div class="border-t border-gray-200 dark:border-gray-600"></div>--}}
-
-{{--                                        <div class="block px-4 py-2 text-xs text-gray-400">--}}
-{{--                                            {{ __('Cambiar Equipos') }}--}}
-{{--                                        </div>--}}
-
-{{--                                        @foreach (Auth::user()->allTeams() as $team)--}}
-{{--                                            <x-switchable-team :team="$team" />--}}
-{{--                                        @endforeach--}}
-{{--                                    @endif--}}
-{{--                                </div>--}}
-{{--                            </x-slot>--}}
-{{--                        </x-dropdown>--}}
-{{--                    </div>--}}
-{{--                @endif--}}
-
                 <!-- Settings Dropdown -->
                 <div class="ms-3 relative">
                     <x-dropdown align="right" width="48">
@@ -170,7 +103,7 @@
                                 @csrf
 
                                 <x-dropdown-link href="{{ route('logout') }}"
-                                         @click.prevent="$root.submit();">
+                                                 @click.prevent="$root.submit();">
                                     {{ __('Salir') }}
                                 </x-dropdown-link>
                             </form>
@@ -231,43 +164,10 @@
                     @csrf
 
                     <x-responsive-nav-link href="{{ route('logout') }}"
-                                   @click.prevent="$root.submit();">
+                                           @click.prevent="$root.submit();">
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
                 </form>
-
-                <!-- Team Management -->
-{{--                @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())--}}
-{{--                    <div class="border-t border-gray-200 dark:border-gray-600"></div>--}}
-
-{{--                    <div class="block px-4 py-2 text-xs text-gray-400">--}}
-{{--                        {{ __('Manage Team') }}--}}
-{{--                    </div>--}}
-
-{{--                    <!-- Team Settings -->--}}
-{{--                    <x-responsive-nav-link href="{{ route('teams.show', Auth::user()->currentTeam->id) }}" :active="request()->routeIs('teams.show')">--}}
-{{--                        {{ __('Team Settings') }}--}}
-{{--                    </x-responsive-nav-link>--}}
-
-{{--                    @can('create', Laravel\Jetstream\Jetstream::newTeamModel())--}}
-{{--                        <x-responsive-nav-link href="{{ route('teams.create') }}" :active="request()->routeIs('teams.create')">--}}
-{{--                            {{ __('Create New Team') }}--}}
-{{--                        </x-responsive-nav-link>--}}
-{{--                    @endcan--}}
-
-{{--                    <!-- Team Switcher -->--}}
-{{--                    @if (Auth::user()->allTeams()->count() > 1)--}}
-{{--                        <div class="border-t border-gray-200 dark:border-gray-600"></div>--}}
-
-{{--                        <div class="block px-4 py-2 text-xs text-gray-400">--}}
-{{--                            {{ __('Switch Teams') }}--}}
-{{--                        </div>--}}
-
-{{--                        @foreach (Auth::user()->allTeams() as $team)--}}
-{{--                            <x-switchable-team :team="$team" component="responsive-nav-link" />--}}
-{{--                        @endforeach--}}
-{{--                    @endif--}}
-{{--                @endif--}}
             </div>
         </div>
     </div>
