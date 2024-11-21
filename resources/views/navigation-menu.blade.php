f9daeee35836aacf5f37f7d6029f9d73272db3ea
Angel Leonardo Bianco <angel.leonardo.bianco@gmail.com>
2024-11-21 13:45:46 -0300

    menu var

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -22,7 +22,7 @@
 
 
             <div class="hidden sm:flex sm:items-center sm:ms-6">
-                <x-dropdown align="right" width="48" style="margin-right:10 px;">
+                <x-dropdown align="right" width="48" style="margin-right: 10px;">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
                             {{ __('Teléfonos') }}
