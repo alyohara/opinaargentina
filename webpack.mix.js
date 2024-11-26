baabfce80097fa76878e23768ce6ebf987da75b4
Angel Leonardo Bianco <angel.leonardo.bianco@gmail.com>
2024-11-26 12:55:39 -0300

    a

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,16 @@
 const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .options({
+        hmrOptions: {
+            host: 'localhost',
+            port: 5176
+        }
+    });
+
+if (mix.inProduction()) {
+    mix.version();
+} else {
+    mix.sourceMaps();
+}
