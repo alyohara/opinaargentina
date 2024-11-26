2d215df1556f9ed9c9e8ff8d21def94752a53984
Angel Leonardo Bianco <angel.leonardo.bianco@gmail.com>
2024-11-26 13:02:59 -0300

    s

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,12 +26,13 @@
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 
     <!-- Compiled CSS -->
-    <link href="http://201.220.23.104:50089/css/app.css" rel="stylesheet">
+    <link href="{{ asset('assets/css/app.css') }}" rel="stylesheet">
+
 
     <!-- Livewire Styles -->
     @livewireStyles
 
-    <script src="http://201.220.23.104:50089/js/app.js" defer></script>
+    <script src="{{ asset('assets/js/app.js') }}" defer></script>
 </head>
 <body class="font-sans antialiased">
 <x-banner />
