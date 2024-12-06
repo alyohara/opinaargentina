657544b6ada05e8a6620a1c476aff7738348e7c1
Angel Leonardo Bianco <angel.leonardo.bianco@gmail.com>
2024-12-06 10:03:08 -0300

    ananlitycs command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,6 +7,9 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    protected $commands = [
+        \App\Console\Commands\CalculateAnalytics::class,
+    ];
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('analytics:calculate')->daily();
