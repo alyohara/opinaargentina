80800c74fd823f56a86152dab7bd973e81dc2e36
Angel Leonardo Bianco <angel.leonardo.bianco@gmail.com>
2024-12-06 13:12:15 +0000

    analytics commando

---
+++ app/Console/Commands/CalculateAnalytics2.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class CalculateAnalytics2 extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:calculate-analytics2';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
