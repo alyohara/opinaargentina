9968980e4ace594ae9a98de4d7b0c1d928c33ebf
Angel Leonardo Bianco <angel.leonardo.bianco@gmail.com>
2024-12-11 08:29:44 -0300

    export query

--- app/Exports/TelsExport.php
+++ app/Exports/TelsExport.php
@@ -2,68 +2,77 @@
 
 namespace App\Exports;
 
-use App\Models\Telefono;
-use Couchbase\View;
-use Maatwebsite\Excel\Concerns\FromQuery;
-use Maatwebsite\Excel\Concerns\WithChunkReading;
-//use Maatwebsite\Excel\Concerns\WithHeadings;
-//use Maatwebsite\Excel\Concerns\WithMapping;
-use Maatwebsite\Excel\Concerns\FromCollection;
-use Maatwebsite\Excel\Concerns\FromView;
 use Illuminate\Contracts\View\View as ViewContract;
 use Illuminate\Database\Eloquent\Builder;
+use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\Exportable;
 
-class TelsExport implements FromView
+class TelsExport implements FromQuery, WithMapping, WithHeadings
 {
-    protected $data;
+    use Exportable;
 
-    public function __construct($data)
+    protected $query;
+
+    /**
+     * Constructor to accept a query builder instance.
+     *
+     * @param Builder $query
+     */
+    public function __construct(Builder $query)
     {
-        $this->data = $data;
+        $this->query = $query;
     }
 
-    public function collection()
+    /**
+     * Query method for streaming data.
+     *
+     * @return Builder
+     */
+    public function query()
     {
-        return $this->data;
+        return $this->query;
     }
 
-//    public function headings(): array
-//    {
-//        return [
-//            'Telefono',
-//            'Localidad',
-//        ];
-//    }
-//
-//    public function map($telefono): array
-//    {
-//        return [
-//            $telefono->telefono,
-//            $telefono->city->name,
-//        ];
-//    }
+    /**
+     * Define the headings for the exported file.
+     *
+     * @return array
+     */
+    public function headings(): array
+    {
+        return [
+            'Telefono',
+            'Localidad',
+        ];
+    }
 
-    public function view(): ViewContract
+    /**
+     * Map each row of data to the desired format.
+     *
+     * @param mixed $telefono
+     * @return array
+     */
+    public function map($telefono): array
     {
         $formattedData = [];
 
-        foreach ($this->data as $telefono) {
-            if ($telefono->movil) {
-                $formattedData[] = [
-                    'telefono' => $telefono->movil,
-                    'localidad' => $telefono->city->name,
-                ];
-            }
-            if ($telefono->telefono) {
-                $formattedData[] = [
-                    'telefono' => $telefono->telefono,
-                    'localidad' => $telefono->city->name,
-                ];
-            }
+        if ($telefono->movil) {
+            $formattedData[] = [
+                'telefono' => $telefono->movil,
+                'localidad' => $telefono->city->name ?? '',
+            ];
         }
 
-        return view('exports.telefonosSimplificado', [
-            'telefonos' => $formattedData
-        ]);
+        if ($telefono->telefono) {
+            $formattedData[] = [
+                'telefono' => $telefono->telefono,
+                'localidad' => $telefono->city->name ?? '',
+            ];
+        }
+
+        // Handle cases where both "movil" and "telefono" exist
+        return count($formattedData) > 0 ? reset($formattedData) : ['telefono' => '', 'localidad' => ''];
     }
 }
