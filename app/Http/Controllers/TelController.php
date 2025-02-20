79b8abae1b7ac30cff38e844834f34e6255856a7
Angel Leonardo Bianco <angel.leonardo.bianco@gmail.com>
2025-02-20 12:45:06 -0300

    tels 0.6.6

--- app/Http/Controllers/TelController.php
+++ app/Http/Controllers/TelController.php
@@ -53,7 +53,7 @@ class TelController extends Controller
 //        }
 
         $tels = $query->cursorPaginate(100);
-        dd($tels);
+        //dd($tels);
         $provincias = $states;
         $localidades = $cities;
         $selectedProvincia = $selectedState;
