d526850db739539b3d76cdadcda5d6d93b0b1320
Angel Leonardo Bianco <angel.leonardo.bianco@gmail.com>
2025-03-02 21:39:53 -0300

    export job 0.5.1

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 //Route::get('telefonos/export/', [TelefonoController::class, 'export'])->name('telefonos.export');
-Route::post('telefonos/export/', [TelefonoController::class, 'export'])->name('telefonos.export');
+Route::post('telefonos/export/', [TelController::class, 'export'])->name('telefonos.export');
 
 
 Route::middleware([
