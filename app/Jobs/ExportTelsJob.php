98177fb62e1228bcbd24d2f3d1a31e2f3f53ca4f
Angel Leonardo Bianco <angel.leonardo.bianco@gmail.com>
2025-03-04 11:34:11 -0300

    expror job 0.6.5.1

--- app/Jobs/ExportTelsJob.php
+++ app/Jobs/ExportTelsJob.php
@@ -5,6 +5,7 @@ namespace App\Jobs;
 use App\Models\Tel;
 use App\Models\Localidad;
 use App\Models\Export;
+use App\Exports\TelsExport;
 use Maatwebsite\Excel\Facades\Excel;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -12,9 +13,9 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Collection;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
-use Illuminate\Support\Facades\DB;
 
 class ExportTelsJob implements ShouldQueue
 {
@@ -26,7 +27,6 @@ class ExportTelsJob implements ShouldQueue
     protected $userId;
     protected $fileName;
     protected $tipoTelefono;
-
     protected $chunkSize = 150000; // Adjust as needed
 
     /**
@@ -52,6 +52,20 @@ class ExportTelsJob implements ShouldQueue
     public function handle()
     {
         ini_set('max_execution_time', 7200);
+        $query = Tel::query()->select('nro_telefono', 'localidad_id');
+
+        if ($this->stateId) {
+            $cityIds = Localidad::where('provincia_id', $this->stateId)->pluck('id');
+            $query->whereIn('localidad_id', $cityIds);
+        }
+
+        if ($this->cityId) {
+            $query->where('localidad_id', $this->cityId);
+        }
+
+        if ($this->tipoTelefono) {
+            $query->where('tipo_telefono', $this->tipoTelefono);
+        }
 
         // Save export details as "procesando"
         $export = new Export();
@@ -63,87 +77,42 @@ class ExportTelsJob implements ShouldQueue
 
         try {
             if ($this->quantity <= $this->chunkSize) {
-                //if is small enough to avoid problems, it runs as before.
-                $query = Tel::query()->select('nro_telefono', 'localidad_id');
-                if ($this->stateId) {
-                    $cityIds = Localidad::where('provincia_id', $this->stateId)->pluck('id');
-                    $query->whereIn('localidad_id', $cityIds);
-                }
-
-                if ($this->cityId) {
-                    $query->where('localidad_id', $this->cityId);
-                }
-
-                if ($this->tipoTelefono) {
-                    $query->where('tipo_telefono', $this->tipoTelefono);
-                }
-
-                // Randomize the query
-                $data = $query->inRandomOrder()->limit($this->quantity)->get();
-
-                Excel::store(new \App\Exports\TelsExport($data), $this->fileName, 'public');
-                $export->file_size = Storage::disk('public')->size($this->fileName);
-                $export->status = 'terminado';
-                $export->job_ended_at = now();
-                $export->save();
-                return;
+                $data = $query->limit($this->quantity)->get();
+                Excel::store(new TelsExport($data), $this->fileName, 'public');
             } else {
                 $numChunks = ceil($this->quantity / $this->chunkSize);
+                $tempFiles = [];
                 $baseFileName = pathinfo($this->fileName, PATHINFO_FILENAME);
                 $extension = pathinfo($this->fileName, PATHINFO_EXTENSION);
-                $tempFiles = [];
 
                 for ($i = 0; $i < $numChunks; $i++) {
                     $offset = $i * $this->chunkSize;
-                    //dispatch a job for every chunk
-                    ExportTelChunkJob::dispatch($this->stateId, $this->cityId, $offset, $this->chunkSize, $this->userId, $baseFileName, $extension, $i, $this->tipoTelefono, true);
-                    $tempFiles[] = "{$baseFileName}_part_{$i}.{$extension}";
-
+                    $chunkData = $query->skip($offset)->take($this->chunkSize)->get();
+                    $tempFileName = "{$baseFileName}_part_{$i}.{$extension}";
+                    $tempFiles[] = $tempFileName;
+                    Excel::store(new TelsExport($chunkData), $tempFileName, 'public');
                 }
-                //wait for all jobs to be finished
-                $this->waitForChunkJobs($tempFiles);
                 $this->mergeExcelFiles($tempFiles, $this->fileName);
-
                 // Delete temp files
                 foreach ($tempFiles as $tempFile) {
                     Storage::disk('public')->delete($tempFile);
                 }
-
-                $export->file_size = Storage::disk('public')->size($this->fileName);
-                $export->status = 'terminado';
             }
+            $export->file_size = Storage::disk('public')->size($this->fileName);
+            $export->status = 'terminado';
         } catch (\Exception $e) {
             $export->status = 'error';
-
             // Delete temp files if there was an error
             if(isset($tempFiles)){
                 foreach ($tempFiles as $tempFile) {
                     Storage::disk('public')->delete($tempFile);
                 }
             }
+
         }
 
         $export->job_ended_at = now();
         $export->save();
-    }
-    protected function waitForChunkJobs($tempFiles)
-    {
-        // Check if all chunk files exist before proceeding
-        while (true) {
-            $allFilesExist = true;
-            foreach ($tempFiles as $tempFile) {
-                if (!Storage::disk('public')->exists($tempFile)) {
-                    $allFilesExist = false;
-                    break;
-                }
-            }
-
-            if ($allFilesExist) {
-                break; // All files exist, exit the loop
-            }
-
-            sleep(5); // Wait for 5 seconds before checking again
-        }
     }
 
     protected function mergeExcelFiles(array $filePaths, string $outputFileName)
